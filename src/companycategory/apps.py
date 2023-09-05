d0b4af039f3eee11c2009dbb6c2504bc1f5b7e5e
takumi-pro <takumidiary.0927@gmail.com>
2023-09-05 19:28:44 +0900

    auto: Generated code

---
+++ src/companycategory/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CompanycategoryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'companycategory'
--- src/urls.py
+++
@@ -1,4 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-
-router = DefaultRouter()
